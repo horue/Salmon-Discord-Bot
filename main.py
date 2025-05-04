934d06cf737c9623d9bd4047b415814da4c70fb7
Jorge <jorgejapao00@gmail.com>
2025-05-04 12:09:46 -0300

    Bot main structure created

---
+++ main.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from tokens import bot_token
+
+
+
+## Bot Connection ##
+
+
+intents = discord.Intents.default()
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix = '-', case_insensitive = True, activity=discord.CustomActivity(name="Use '--help' for help"), status=discord.Status.online, intents=intents)
+tree = bot.tree
+
+@bot.event
+async def on_ready():
+  await tree.sync()
+  print('Entramos como {0.user}' . format(bot))
+
+
+
+## General Commands ##
+
+
+@tree.command(name='test', description="This is a test command.")
+async def teste(interaction: discord.Interaction):
+  await interaction.response.send_message("Testing")
+
+@bot.command()
+async def ping(ctx='ping'):
+  await ctx.send("pong!")
+
+
+
+
+
+
+bot.run(bot_token)
