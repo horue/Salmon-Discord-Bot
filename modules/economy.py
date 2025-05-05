3dfc1d59b0123ece7b6ffe2a6b0caccb4d1ee49b
Jorge <jorgejapao00@gmail.com>
2025-05-05 20:12:51 -0300

    command to add money created

--- modules/economy.py
+++ modules/economy.py
@@ -8,6 +8,29 @@ async def create_wallet(server_id, bank, wallet, id):
     print(request)
     print(request.text)
 
+
+async def add_money(server_id, user_id, amount):
+    request = requests.get(f'{firebase}/Servidores/{server_id}.json')
+    print(request)
+    found = False
+    dic_request = request.json()
+    for user_id_i in dic_request:
+        user = dic_request[user_id_i]['ID']
+        if user == user_id:
+            current_wallet = dic_request[user_id_i]['Wallet']
+            new_wallet = current_wallet + amount
+            url = f'{firebase}/Servidores/{server_id}/{user_id_i}.json'
+            response = requests.patch(url, json={"Wallet": new_wallet})
+            if response.status_code == 200:
+                print(f'User: {user}, Wallet: {new_wallet}, Bank: {dic_request[user_id_i]["Bank"]}')
+            else:
+                print('Erro ao atualizar a carteira.')
+            found = True
+            break
+    if not found:
+        print('No wallet found for this user.')
+
+
 async def check_money(server_id, user_id):
     request = requests.get(f'{firebase}/Servidores/{server_id}.json')
     print(request)
@@ -27,6 +50,6 @@ async def check_money(server_id, user_id):
 
 
 if __name__ == '__main__':
-    #asyncio.run(create_wallet(123, 10, 10, 999))
+    asyncio.run(add_money(123,999,4500))
     asyncio.run(check_money(123, 456))
     asyncio.run(check_money(123, 789))
