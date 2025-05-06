19ff765912a87289d67195d58b1ded17454406f2
Jorge <jorgejapao00@gmail.com>
2025-05-06 20:31:42 -0300

    now mentions the discord user that wallet's was checked

--- modules/economy.py
+++ modules/economy.py
@@ -1,7 +1,11 @@
 import json
 import requests
 import asyncio
-from firebase import link as firebase
+import discord
+from discord.ext import commands
+from discord import app_commands
+from discord.ext.commands import Context
+from discord import Interaction
 
 async def create_wallet(server_id, bank, wallet, id):
     request = requests.post(f'{firebase}/Servidores/{server_id}.json', json={'ID': id,'Wallet': wallet, 'Bank': bank})
@@ -31,22 +35,29 @@ async def add_money(server_id, user_id, amount):
         print('No wallet found for this user.')
 
 
-async def check_money(server_id, user_id):
+async def check_money(ctx, server_id, user_id):
     request = requests.get(f'{firebase}/Servidores/{server_id}.json')
     print(request)
-    found = False
     dic_request = request.json()
+    if dic_request is None:
+        await ctx.send("No data found for this server.")
+        await create_wallet(server_id=server_id, bank=0, wallet=0, id=user_id)
+        await ctx.send('New wallet created.')
+        return
+    found = False
     for user_id_i in dic_request:
         user = dic_request[user_id_i]['ID']
         if user == user_id:
             wallet = dic_request[user_id_i]['Wallet']
             bank = dic_request[user_id_i]['Bank']
-            print(f'User: {user}, Wallet: {wallet}, Bank: {bank}')
+            await ctx.send((f'User: <@{user}>\nWallet: {wallet}\nBank: {bank}'))
             found = True
+            break
     if not found:
-        print('No wallet found for this user.')
+        await ctx.send('No wallet found for this user.')
         await create_wallet(server_id=server_id, bank=0, wallet=0, id=user_id)
-        print('New wallet created.')
+        await ctx.send('New wallet created.')
+
 
 
 if __name__ == '__main__':
